ca2b1b5a7d67dfb95c9bcc9e627fe2e02c22b198
Vinícius Souza <tradutorvinicius@gmail.com>
2023-04-24 18:28:27 -0300

    feat: modifica posição do componente Feedback para o diretório pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
-import Feedback from './components/Feedback';
 
 class App extends Component {
   render() {
@@ -13,7 +12,6 @@ class App extends Component {
         <Route exact path="/" component={ Login } />
         <Route exact path="/game" component={ Game } />
         <Route exact path="/settings" component={ Settings } />
-        <Route exact path="/feedback" component={ Feedback } />
       </Switch>
     );
   }
