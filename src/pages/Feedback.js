dca7b7a53afc7395c921912fe4294f14d3776b23
leocaridade <leocaridade@hotmail.com>
2023-04-25 19:33:50 -0400

    feat: resolve o requisito 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -24,6 +24,12 @@ class Feedback extends Component {
         >
           Play Again
         </button>
+        <button
+          data-testid="btn-ranking"
+          onClick={ () => history.push('/ranking') }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
